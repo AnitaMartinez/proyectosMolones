77995b447b688627ca90b03cc055505d8ef642c8
Ana Martinez <amartinezaguilar6@gmail.com>
2025-04-24 11:23:05 +0200

    app

--- web/src/components/App.jsx
+++ web/src/components/App.jsx
@@ -1,7 +1,14 @@
 import "../scss/App.scss";
 
 function App() {
-  return <p>gfsgdsfgfdsg</p>;
+    return (
+        <>
+            <header><h1>Pagina web</h1></header>
+            <main>
+                <p>Ex sint id tempor exercitation proident cupidatat aliquip laborum in. Duis et culpa est consequat officia. Cillum incididunt commodo adipisicing cupidatat eu. Anim ullamco amet proident pariatur occaecat laboris et fugiat aliquip in sunt aliquip ex irure. Ipsum eu tempor laborum fugiat est elit cillum sit aliqua anim occaecat aliqua. Consequat veniam exercitation amet ut sunt irure enim.</p>
+            </main>
+        </>
+    );
 }
 
 export default App;
