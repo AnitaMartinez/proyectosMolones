e3491a47d365ffeeddf3890a5749beb381fc1706
Ana Martinez <amartinezaguilar6@gmail.com>
2025-04-24 11:56:05 +0200

    Update title

--- web/src/components/App.jsx
+++ web/src/components/App.jsx
@@ -17,6 +17,7 @@ function App() {
         <>
             <header><h1>Pagina web</h1></header>
             <main>
+                <h2>Este es el main</h2>
                 <p>Ex sint id tempor exercitation proident cupidatat aliquip laborum in. Duis et culpa est consequat officia. Cillum incididunt commodo adipisicing cupidatat eu. Anim ullamco amet proident pariatur occaecat laboris et fugiat aliquip in sunt aliquip ex irure. Ipsum eu tempor laborum fugiat est elit cillum sit aliqua anim occaecat aliqua. Consequat veniam exercitation amet ut sunt irure enim.</p>
             </main>
         </>
